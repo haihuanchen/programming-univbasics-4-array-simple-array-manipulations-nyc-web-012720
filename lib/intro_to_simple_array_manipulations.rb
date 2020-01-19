e5e0fed9331e192eba408607ee6f300fd3d38b76
Sam Chen <haihuan.chen@hotmail.com>
2020-01-19 07:21:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+def using_push(array, string)
+  updated_array = array.push(string)
+end
+
+def using_unshift(array, string)
+  updated_array = array.unshift(string)
+end
+
+def using_pop(array)
+  updated_array = array.pop
+end
+
+def pop_with_args(array, string)
+  updated_array = array.pop(string)
+end
+
+def using_shift(array)
+  updated_array = array.shift
+end
+
+def shift_with_args(array, string)
+  updated_array = array.shift(string
+end
+
+def using_concat(array1, array2)
+  array1 = array1.concat(array2)
